8c8afed2f69fb18ecb8bb44579aba16cc320f902
Ferdi <efcm@vdwerf.eu>
2011-04-15 11:43:10 +0200

    publicationNews.class.php: * rewritten itemDetail($pConf) * bugfixes:   - releaseAt was not used   - maxPages was initialized incorrectly * performace boost by removing several array lookups
    default.tpl:
    * using isNews
    * showing a decent message when a news item can not be shown

--- inc/modules/publication/publicationNews.class.php
+++ inc/modules/publication/publicationNews.class.php
@@ -1,116 +1,165 @@
 <?php
 
-class publicationNews {
+class publicationNews
+{
     
-    public static function itemDetail( $pConf ){
+    public static function itemDetail( $pConf )
+    {
         global $user;
         
+        // Get important variables from config
+        $replaceTitle = $pConf['replaceTitle']+0 == 1; // Ensure it's set
+        $categoryRsn = $pConf['category_rsn'];
+        $allowComments = $pConf['allowComments']+0;
+        $template = $pConf['template'];
         
-        if( $pConf['replaceTitle'] == "" )
-            $pConf['replaceTitle'] = "1";
-
-        $id = getArgv('e2')+0;
-        if( $id > 0 ){
-            $cats = implode($pConf['category_rsn'], ",");
+        // News item to show
+        $rsn = getArgv('e2')+0;
+        
+        if( $rsn > 0 )
+        {
+            // Create category where clause
+            $whereCategories = "";
+            if(count($categoryRsn))
+                $whereCategories = "AND category_rsn IN (".implode(",", $categoryRsn).")";
             
-            if( $cats != "" )
-                $where = " category_rsn IN ($cats) AND ";
+            // Create query
+            $now = time();
+            $sql = "
+                SELECT
+                    n.*,
+                    c.title as categoryTitle
+                FROM
+                    %pfx%publication_news n,
+                    %pfx%publication_news_category c
+                WHERE
+                    1=1
+                    $whereCategories
+                    AND n.deactivate = 0
+                    AND c.rsn = n.category_rsn
+                    AND n.rsn = $rsn
+                    AND (n.releaseAt = 0 OR n.releaseAt < $now)
+            ";
             
-            $news = dbExFetch("SELECT n.*, c.title as categoryTitle FROM %pfx%publication_news n, %pfx%publication_news_category c WHERE
-            $where deactivate = 0 and category_rsn = c.rsn AND n.rsn = ".$id, 1);
-            if( $news['rsn'] != $id )
-                return "[Errror:ext:publication] Invalid news id.";
-
-            if( $pConf['replaceTitle'] == "1" ){
-                kryn::$page['title'] = $news['title'];
-            }
-
-            if( $pConf['allowComments'] == 1 && $news['deactivateComments'] != 1 ){
-
-                if( getArgv('publication-add-comment') == 1 ){
+            $news = dbExfetch($sql, 1);
+            
+            // Is it a valid news row
+            $isNews = $news !== false;
+            tAssign('isNews', $isNews);
+            
+            if($isNews)
+            {
+                // Set title if allowed
+                if($replaceTitle)
+                    kryn::$page['title'] = $news['title'];
                     
-                    $name = ($user->user_rsn == 0 ) ? getArgv('name',1) : $user->user['username'];
-                    if( $name != ""  ){
-                        dbInsert('publication_comments', array(
-                            'parent_rsn' => $news['rsn'],
-                            'owner_rsn' => $user->user_rsn,
-                            'owner_username' => $name,
-                            'created' => time(),
-                            'ip' => $_SERVER['REMOTE_ADDR'],
-                            'session_id' => $user->sessionid,
-                            'subject',
-                            'website',
-                            'email',
-                            'message'
-                        ));
-                        
-                        self::updateCommentsCount( $news['rsn'] );
-                        $news['commentscount']++;
+                // Handle comment calls
+                if($allowComments && $news['deactivateComments']+0 == 0)
+                {
+                    if(getArgv('publication-add-comment')+0)
+                    {
+                        $name = $user->user_rsn == 0 ? getArgv('name', 1) : $user->user['username'];
+                        if($name != "")
+                        {
+                            dbInsert(
+                                'publication_comments',
+                                array(
+                                    'parent_rsn' => $rsn,
+                                    'owner_rsn' => $user->user_rsn,
+                                    'owner_username' => $name,
+                                    'created' => time(),
+                                    'ip' => $_SERVER['REMOTE_ADDR'],
+                                    'session_id' => $user->sessionid,
+                                    'subject',
+                                    'website',
+                                    'email',
+                                    'message'
+                                )
+                            );
+                            self::updateCommentsCount($rsn);
+                            $news['commentscount']++;
+                        }
                     }
+                    
+                    // Default itemsPerPage if not set
+                    $itemsPerPage = $pConf['itemsPerPage']+0;
+                    if(!$itemsPerPage)
+                        $itemsPerPage = 15;
+                        
+                    // From which comment page are we looking?
+                    $page = getArgv('e3')+0;
+                    if($page == 0)
+                        $page = 1;
+                    
+                    // Default max pages if not set
+                    $maxPages = $pConf['maxPages']+0;
+                        
+                    // Set comments start
+                    $start = $itemsPerPage * $page - $itemsPerPage;
+                    
+                    // Count comments
+                    $sqlCount = "
+                        SELECT
+                            count(*) as commentsCount
+                        FROM
+                            %pfx%publication_comments
+                        WHERE
+                            parent_rsn = $rsn
+                    ";
+                    $countRow = dbExfetch($sqlCount, 1);
+                    $count = $countRow['commentsCount'];
+                    tAssign('commentsCount', $count);
+                
+                    // Set amount of pages
+                    $pages = 1;
+                    if($count && $itemsPerPage)
+                        $pages = ceil($count / $itemsPerPage);
+                    
+                    // Update max pages when needed
+                    if(!$maxPages)
+                        $pConf['maxPages'] = $pages;
+                   
+                    tAssign('pages', $pages);
+                    tAssign('currentCommentPage', $page);
+                    
+                    // Fetch comments
+                    $comments = dbTableFetch('publication_comments', -1, "parent_rsn = $rsn LIMIT $start, $itemsPerPage");
+                    if($comments !== false)
+                        tAssign('comments', $comments);
                 }
-
-
-                if( $pConf['itemsPerPage']+0 == 0 )
-                    $pConf['itemsPerPage'] = 15;
-
-                $page = getArgv('e3')+0;
-                $page = ($page==0)?1:$page;
-
-                if( $pConf['maxPages']+0 == 0)
-                    $pConf['maxPages'] = 10;
-
-                if( $page == 1 )
-                    $start = 0;
-                else
-                    $start = ($pConf['itemsPerPage'] * $page) - $pConf['itemsPerPage'];
-
-
-                $sqlCount = "SELECT count(*) as comcount
-                    FROM %pfx%publication_comments WHERE
-                    parent_rsn = ".$news['rsn'];
-                $countRow = dbExfetch( $sqlCount, 1 );
-
-                $count = $countRow['comcount'];
-                tAssign( 'count', $count );
-                $pages = 1;
-                if( $count > 0 && $pConf['itemsPerPage'] > 0 )
-                    $pages = ceil($count/ $pConf['itemsPerPage'] );
-
-                if( $pConf['maxPages']+0 == 0 )
-                    $pConf['maxPages'] = $pages;
-
-                tAssign( 'pages', $pages );
-                tAssign( 'currentCommentsPage', $page );
                 
-                $limit = $pConf['itemsPerPage'].' OFFSET '.$start;
-                $comments = dbTableFetch('publication_comments', -1, 'parent_rsn = '.$news['rsn'].' LIMIT '.$limit );
-                if( $comments )
-                    tAssign('comments', $comments);
-            }
-        
-            $json = json_decode( $news['content'], true );
-            if( $json && $json['contents'] && file_exists('inc/template/'.$json['template']) ){
+                // Retrieve content of news
+                $json = json_decode($news['content'], true);
+                if($json && $json['contents'] && file_exists('inc/template/'.$json['template']))
+                {
+                    $oldContents = kryn::$contents;
+                    kryn::$contents = $json['contents'];
+                    $news['contents'] = tFetch($json['template']);
+                    kryn::$contents = $oldContents;
+                }
                 
-                $oldContents = kryn::$contents;
-                kryn::$contents = $json['contents'];
-                $news['content'] = tFetch($json['template']);
-                kryn::$contents = $oldContents;
-            }
-            
-            $json = json_decode( $news['intro'], true );
-            if( $json && $json['contents'] && file_exists('inc/template/'.$json['template']) ){
+                // Retrieve intro of news
+                $json = json_decode($news['intro'], true);
+                if($json && $json['contents'] && file_exists('inc/template/'.$json['template']))
+                {
+                    $oldContents = kryn::$contents;
+                    kryn::$contents = $json['contents'];
+                    $news['intro'] = tFetch($json['template']);
+                    kryn::$contents = $oldContents;
+                }
                 
-                $oldContents = kryn::$contents;
-                kryn::$contents = $json['contents'];
-                $news['intro'] = tFetch($json['template']);
-                kryn::$contents = $oldContents;
+                tAssign('news', $news);
             }
-            
-            tAssign('news', $news);
-            tAssign('pConf', $pConf);
-            kryn::addCss( 'publication/news/css/detail/'.$pConf['template'].'.css' );
-            return tFetch('publication/news/detail/'.$pConf['template'].'.tpl');
+            else 
+                tAssign('isNews', false); // Not found (or not visible)
         }
+        else 
+            tAssign('isNews', false); // Not a valid news item
+
+        // Assign config and load template
+        tAssign('pConf', $pConf);
+        kryn::addCss("publication/news/css/detail/$template.css");
+        return tFetch("publication/news/detail/$template.tpl");
     }
 
     public static function updateCommentsCount( $pNewsRsn ){
