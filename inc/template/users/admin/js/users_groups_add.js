81ae32d9390ce94dec76d8de8a1369db7af716fd
Ferdi <efcm@vdwerf.eu>
2011-05-26 12:48:21 +0200

    * Changed window type from windowAdd to custom * Added js file for custom window
    Window has same behaviour as before change.

---
+++ inc/template/users/admin/js/users_groups_add.js
@@ -0,0 +1,10 @@
+users_users_groups_add = new Class({
+	Extends: ka.windowAdd,
+	
+	initialize: function( pWin )
+	{
+        this.windowAdd = true;
+        this.parent(pWin);
+        logger("bla!");
+    }
+});
